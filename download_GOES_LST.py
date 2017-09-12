b92e1d054b6241b5b5d845d87c9490dd818685e0
Yan Li <yanli0@illinois.edu>
2017-09-11 23:55:42 -0500

    Add urllib2 method

--- download_GOES_LST.py
+++ download_GOES_LST.py
@@ -6,8 +6,7 @@ Need to order data first
 import ftplib
 import os
 
-def download_GOES_LST(id, year=2009, source='GENHEM'):
-   # id = '2584218284'
+def download_GOES_LST(id, year=2009, source='GENHEM',test=False):
     print('download for Year=%d, source=%s'%(year, source))
     os.chdir('data/' + source)
     currdir=os.getcwd()
@@ -16,19 +15,45 @@ def download_GOES_LST(id, year=2009, source='GENHEM'):
     if not os.path.exists(str(year)):
         os.makedirs(str(year))
     os.chdir(str(year))
+    try:
+        ftp = ftplib.FTP('ftp.class.ngdc.noaa.gov') 
+        ftp.login()
 
-    ftp = ftplib.FTP('ftp.class.ngdc.noaa.gov') 
-    ftp.login()
+        ftp.cwd(id + '/001')
+        print('enter OK')
+        if not test:
+            fn_list = ftp.nlst()
+            fn_list.sort()
 
-    ftp.cwd(id + '/001')
+            for fn in fn_list:
+                print(fn)
+                ftp.retrbinary("RETR " + fn, open(fn, 'wb').write)
 
-    fn_list = ftp.nlst()
-    fn_list.sort()
-
-    for fn in fn_list:
-        print(fn)
-        ftp.retrbinary("RETR " + fn, open(fn, 'wb').write)
-
-    print('Download task complete, close FTP connection')
-    ftp.quit()  
+            print('Download task complete, close FTP connection')
+        ftp.quit()  
+    except:
+        print ('%s Error'%id)
     os.chdir('../../../')
+
+import urllib2
+import shutil
+import pandas as pd 
+def get_file(url):
+    req = urllib2.urlopen(url)
+    file = s.split('/')[-1]
+    with open(file, 'wb') as fp:
+        shutil.copyfileobj(req, fp)
+
+def get_file_urllib2(id, year=2009, source='GENHEM')
+    ftp_name = 'ftp://ftp.class.ncdc.noaa.gov/'
+   # id = '2585819234'
+    fn = pd.read_csv(id + '.txt', sep=' ', header=None)
+
+    if not os.path.exists(str(year)):
+        os.makedirs(str(year))
+    os.chdir(str(year))
+    
+    for i in range(fn.shape[0]):
+        s = ftp_name + id + fn[1][i]
+        print('dowloading %s'%s)
+        get_file(s)
