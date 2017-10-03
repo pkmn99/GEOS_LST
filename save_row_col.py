e7f532e143d6139fbb19f6a6e27a2ebd84b5ba24
yanli <yanli.geo@gmail.com>
2017-10-03 17:53:15 -0500

    Use fixed boundary

--- save_row_col.py
+++ save_row_col.py
@@ -1,13 +1,31 @@
-import xarray as xr
+import numpy as np
+import pandas as pd
 from affine import Affine
 
-fn = 'gsipL3_g13_GENHEM_2012359_1245.nc'
-ds = xr.open_dataset(fn)
+#def US_boundary_table():
+# US lat/lon boundary
+lat_north = 50
+lat_south = 20
+lon_west = -125
+lon_east = -70
+res = 0.125
 
+lat = np.arange(lat_north-res/2, lat_south-res/2 -0.001, -res)
+lon = np.arange(lon_west+res/2, lon_east + res/2 + 0.001, res)
 
-a = Affine(0.125,0,-132.0625,0,-0.125,59.9375)
+lon_mesh, lat_mesh = np.meshgrid(lon, lat)
+
+a = Affine(0.125,0,lon_west,0,-0.125,lat_north)
+
 # get col and row number
-ds['col'], ds['row'] = ~a * (ds['lon_cell'], ds['lat_cell'])
-# need to floor to get integer col and row
-ds[['row','col']].to_netcdf('row_col.nc')
-print('row and col saved to file')
+col, row = ~a * (lon_mesh.flatten(), lat_mesh.flatten())
+
+d = {'lat' : lat_mesh.flatten(),
+     'lon' : lon_mesh.flatten(),
+     'row' : np.int16(row),
+     'col' : np.int16(col),
+     'ind_1d': np.arange(len(row))}
+
+df = pd.DataFrame(d, columns=['lat','lon','row','col','ind_1d'])
+df.to_csv('result/US_row_col_0125deg.csv')
+print('File saved as result/US_row_col_0125deg')
