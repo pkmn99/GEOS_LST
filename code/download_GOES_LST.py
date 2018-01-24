032788b91502d5f9c866c218ac58522b89c48090
Yan Li <yanli0@illinois.edu>
2018-01-24 11:20:41 -0600

    download data

---
+++ code/download_GOES_LST.py
@@ -0,0 +1,69 @@
+"""
+Download GOES LST, using the id 
+download_GOES_LST(id, year=2009, source='GENHEM')
+Need to order data first
+"""
+import ftplib
+import os
+
+def download_GOES_LST(id, year=2009, source='GENHEM',test=False):
+    print('download for Year=%d, source=%s'%(year, source))
+    os.chdir('data/' + source)
+    currdir=os.getcwd()
+    
+    # Create year folder
+    if not os.path.exists(str(year)):
+        os.makedirs(str(year))
+    os.chdir(str(year))
+    try:
+        ftp = ftplib.FTP('ftp.class.ngdc.noaa.gov') 
+        ftp.login()
+
+        ftp.cwd(id + '/001')
+        print('enter OK')
+        if not test:
+            fn_list = ftp.nlst()
+            fn_list.sort()
+
+            for fn in fn_list:
+                print(fn)
+                ftp.retrbinary("RETR " + fn, open(fn, 'wb').write)
+
+            print('Download task complete, close FTP connection')
+        ftp.quit()  
+    except:
+        print ('%s Error'%id)
+    os.chdir('../../../')
+
+
+import urllib2
+import shutil
+import pandas as pd 
+"""
+Use urllib2 to download files. Need to copy file name from the email to a txt that is named 
+after the task number. 
+"""
+def get_file(url):
+    req = urllib2.urlopen(url)
+    file = url.split('/')[-1]
+    with open(file, 'wb') as fp:
+        shutil.copyfileobj(req, fp)
+
+def get_file_urllib2(id, year=2009, source='GENHEM'):
+    ftp_name = 'ftp://ftp.class.ncdc.noaa.gov/'
+    fn = pd.read_csv(id + '.txt', sep=' ', header=None)
+
+    os.chdir('data/' + source)
+    currdir=os.getcwd()
+
+    if not os.path.exists(str(year)):
+        os.makedirs(str(year))
+    os.chdir(str(year))
+    
+    for i in range(fn.shape[0]):
+        s = ftp_name + id + '/' + fn[1][i]
+        print('dowloading %s'%s)
+        get_file(s)
+    print('Download task complete')
+
+    os.chdir('../../../')
