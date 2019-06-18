875a0fc37fc3a1021bb9d663c5af19bafd966c1b
Yan Li <yanli0@illinois.edu>
2019-06-18 07:34:57 -0500

    add GENHME option

--- code/func_GSIP.py
+++ code/func_GSIP.py
@@ -6,7 +6,7 @@ import xarray as xr
 Load hourly raw GSIP data
 """
 def load_hourly_data_for_day(year, day, h=0, head_txt='gsipL3_g13_GENHEM_'):
-    dir_txt = 'data/GENHEM/' + str(year) + '/'
+    dir_txt = '../data/GENHEM/' + str(year) + '/'
 #     dir_txt = 'test_data/'
     fn = dir_txt + head_txt + str(year)+ '%03d'%day + '_%02d'%h + '45.nc'
     try:
@@ -32,12 +32,26 @@ def check_data_boundary():
     df.to_csv('result/gsip_boundary.csv')
 
 # Define US lat/lon boundary
-def US_latlon_boundary():
-    lat_north = 50
-    lat_south = 20
-    lon_west = -125
-    lon_east = -70
+def US_latlon_boundary(type):
     res = 0.125
+    if type=='US':
+        lat_north = 50
+        lat_south = 20
+        lon_west = -125
+        lon_east = -70
+
+    if type=='global':
+        lat_north = 90
+        lat_south = -90
+        lon_west = -180
+        lon_east = 180
+
+    if type=='GENHEM':
+        lat_north = 60
+        lat_south = -30
+        lon_west = -140
+        lon_east = -30
+
     lat = np.arange(lat_north-res/2, lat_south-res/2, -res)
     lon = np.arange(lon_west+res/2, lon_east + res/2, res)
     return lat, lon
@@ -46,20 +60,20 @@ def US_latlon_boundary():
 Convert the GSIP LST data to 2d map
 array = convert_to_map(ds)
 """ 
-def convert_to_map(ds):
+def convert_to_map(ds,var):
     c = (ds['lon_cell']>=lon[0]) & (ds['lon_cell']<=lon[-1]) \
         &(ds['lat_cell']>=lat[-1]) & (ds['lat_cell']<=lat[0])
     
     # Make a dataframe to save points within the US lat/lon boundary
     df_data = pd.DataFrame({'lat': ds['lat_cell'].values[c.values],
                             'lon': ds['lon_cell'].values[c.values],
-                            'lst': ds['lst'].values[c.values]
+                            var: ds[var].values[c.values]
                              })
     # Link it with the references US data frame
-    df_data = df_data.merge(US_table, on=['lat','lon'])
+    df_data = df_data.merge(US_table, on=['lat','lon'],how='right')
         
     map_array = np.zeros([len(lat), len(lon)],dtype=np.int8).flatten() + np.int8(-128)
-    map_array[df_data['ind_1d'].values] = df_data['lst'].values
+    map_array[df_data['ind_1d'].values] = df_data[var].values
     
     return map_array.reshape(len(lat), len(lon))    
 
@@ -67,7 +81,7 @@ def convert_to_map(ds):
 Save GSIP hourly data to netcdf, one month for one file 
 Usage: save_to_netcdf_monthly(year, month)
 """
-def save_to_netcdf_monthly(year, month):
+def save_to_netcdf_monthly(year, month,var,extent='global'):
     # Create a dataframe to save time index
     date = pd.date_range(start='2009/04/01/00:45',end='20170101', freq='h')
     df_time=pd.DataFrame(range(len(date)),index=date)
@@ -78,18 +92,19 @@ def save_to_netcdf_monthly(year, month):
         print('processing %s'%t)
         ds = load_hourly_data_for_day(t.year, t.dayofyear, h=t.hour, head_txt='gsipL3_g13_GENHEM_')
         if ds:
-            lst_array[i,:,:] = convert_to_map(ds)
+            lst_array[i,:,:] = convert_to_map(ds,var)
     # Load a sample data to get attribute 
     ds_sample = load_hourly_data_for_day(2012, 1, h=0, head_txt='gsipL3_g13_GENHEM_')
 
     foo = xr.DataArray(lst_array, coords=[time_month.index, lat, lon], dims=['time','latitude','longitude'], 
-                       attrs=ds_sample.lst.attrs, name='lst')
-    foo.to_netcdf('result/gsipL3_g13_GENHEM_%d_%02d.nc'%(year,month))
-    print('Netcdf file gsipL3_g13_GENHEM_%d_%02d.nc saved'%(year,month))
+                       attrs=ds_sample[var].attrs, name=var)
+    foo.to_netcdf('../result/%s_%s_%d_%02d.nc'%(extent,var, year,month))
+    print('Netcdf file %s_%d_%02d.nc saved'%(extent,year,month))
 
 if __name__ == '__main__':
-    lat, lon = US_latlon_boundary()
-    US_table = pd.read_csv('result/US_row_col_0125deg.csv')
-    year = 2014
-    for month in range(4, 13):
-        save_to_netcdf_monthly(year, month)
+    extent='GENHEM'
+    lat, lon = US_latlon_boundary(extent)
+    US_table = pd.read_csv('../result/%s_row_col_0125deg.csv'%extent)
+    for year in range(2011,2012):
+        for month in range(5, 9):
+            save_to_netcdf_monthly(year, month,'frac_total_cld',extent=extent)
--- code/save_row_col.py
+++ code/save_row_col.py
@@ -6,13 +6,25 @@ from affine import Affine
 Save US lat/lon boundary and the corresponding row and col number
 to a csv file, which can be used for other functions 
 """
-def US_boundary_table():
-    # US lat/lon boundary
-    lat_north = 50
-    lat_south = 20
-    lon_west = -125
-    lon_east = -70
+def US_boundary_table(type):
     res = 0.125
+    if type=='US':
+        lat_north = 50
+        lat_south = 20
+        lon_west = -125
+        lon_east = -70
+
+    if type=='global':
+        lat_north = 90
+        lat_south = -90
+        lon_west = -180
+        lon_east = 180
+
+    if type=='GENHEM':
+        lat_north = 60
+        lat_south = -30
+        lon_west = -140
+        lon_east = -30
     
     lat = np.arange(lat_north-res/2, lat_south-res/2, -res)
     lon = np.arange(lon_west+res/2, lon_east + res/2, res)
@@ -31,8 +43,9 @@ def US_boundary_table():
          'ind_1d': np.arange(len(row))}
     
     df = pd.DataFrame(d, columns=['lat','lon','row','col','ind_1d'])
-    df.to_csv('result/US_row_col_0125deg.csv')
-    print('File saved as result/US_row_col_0125deg')
+#    df.to_csv('result/US_row_col_0125deg.csv')
+    df.to_csv('../result/%s_row_col_0125deg.csv'%type)
+    print('File saved as result/%s_row_col_0125deg'%type)
 
 if __name__=="__main__":
-    US_boundary_table()
+    US_boundary_table('GENHEM')
